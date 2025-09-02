5c3dbd0fead3d0e41b566e11efffb66814d3a715
ines Tmimi <ines.tmimi@ensit.u-tunis.tn>
2025-09-02 11:19:10 +0100

    exercice 1

--- docker-example-app/app.js
+++ docker-example-app/app.js
@@ -50,6 +50,6 @@ app.get('/health', (req, res) => {
 
 // Start server
 app.listen(PORT, () => {
-    console.log(`🚀 Server is running on http://localhost:${PORT}`);
-    console.log(`📋 Environment: ${process.env.NODE_ENV || 'development'}`);
+    console.log(` Server is running on http://localhost:${PORT}`);
+    console.log(` Environment: ${process.env.NODE_ENV || 'development'}`);
 });
